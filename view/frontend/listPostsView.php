3140f70a2984a02e42213fb0429421317e62a0ec
v-germain <vassili.germain@gmail.com>
2019-09-20 14:17:10 +0200

    added image by post + starting css

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -1,30 +1,30 @@
 <?php $title = "Mon blog"; ?>
 
 <?php ob_start(); ?>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
- 
-        
-        <?php
-        while ($data = $posts->fetch())
-        {
-        ?>
-            <div class="news">
-                <h3>
-                    <?= htmlspecialchars($data['title']) ?>
-                    <em>le <?= $data['creation_date_fr'] ?></em>
-                </h3>
-                
-                <p>
-                    <?= nl2br(/*htmlspecialchars*/($data['content'])) ?>
-                    <br />
-                    <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
-                </p>
-            </div>
-        <?php
-        }
-        $posts->closeCursor();
-        ?>
+<h1>Mon super blog !</h1>
+<p>Derniers billets du blog :</p>
+
+<div class="post_container">
+
+<?php
+while ($data = $posts->fetch()) {
+    ?>
+    
+    <div class="card" style="width: 18rem;">
+        <img src="public/images/ch<?= $data['id'] ?>.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h3 class="card-title"> <?= htmlspecialchars($data['title']) ?></h3>
+            <p class="card-text"> <?= nl2br(/*htmlspecialchars*/($data['content'])) ?> <br />
+            <em>le <?= $data['creation_date_fr'] ?></em></p>
+            <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-primary">Commentaires</a>
+        </div>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
 <?php $content = ob_get_clean(); ?>
 
-<?php require ('template.php'); ?>
+</div>
+
+<?php require('template.php'); ?>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -15,18 +15,23 @@
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="index.php">Acceuil</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Features</a>
+                <!--<a class="nav-item nav-link active" href="index.php">Acceuil<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Features</a>-->
                 <a class="nav-item nav-link" href="index.php?action=admin">Connexion</a>
             </div>
         </div>
     </nav>
+
+    <div class='banner'>
+        <h1>Billet simple pour l'Alaska</h1>
+        <h2>Ne ratez pas le nouveau roman choc de Jean Forteroche!</h2>
+    </div>
 </header>
 
 <body>
